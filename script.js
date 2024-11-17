879076355bd58590ec583badbd9129f3a78aa538
Prince Kumar <91559607+prince7858@users.noreply.github.com>
2024-11-18 00:41:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+function updateWindowSize() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    const sizeInfo = document.getElementById('sizeInfo');
+    sizeInfo.querySelector('h1').textContent = `Width: ${width} and Height: ${height}`;
+}
+updateWindowSize();
+window.addEventListener("resize",updateWindowSize)
